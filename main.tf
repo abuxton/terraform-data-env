43d30b5295dd223b7a64723debaadc36ab9c4cdf
Adam Buxton <buxton.adam@gmail.com>
2023-08-02 12:31:17 +0100

    .

--- main.tf
+++ main.tf
@@ -2,9 +2,8 @@
 # Resources
 # ------------------------------------------------------------------------------
 
-resource "commandpersistence_cmd" "which_python" {
-  program = ["shell", "which python"]
-  path    = "/usr/bin:/usr/local/bin:/bin:/usr/sbin:/sbin"
+/* resource "commandpersistence_cmd" "which_python" {
+  program = ["which", "python"]
 
 }
 resource "commandpersistence_cmd" "which_jq" {
@@ -14,5 +13,5 @@ resource "commandpersistence_cmd" "which_jq" {
 resource "commandpersistence_cmd" "terraform_module_versions" {
   program = ["jq", "\".Modules\" .terraform/modules/modules.json"]
 
-}
+} */
 
