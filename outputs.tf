4287349b0bf4fb135883b0062efd6a9514e8453f
Adam Buxton <buxton.adam@gmail.com>
2023-08-02 12:08:56 +0100

    add providers to init terraform folder

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,11 @@
 # ------------------------------------------------------------------------------
 # Output
 # ------------------------------------------------------------------------------
-output "name_prefix" {
-  value = var.name_prefix
+output "environment_data_all" {
+  value       = data.environment_variables.all
+  description = "All environment variables, derived from Epp0/environment provider"
 }
-
-output "tags" {
-  value = var.tags
+output "commandpersistence_cmd_terraform_module_versions" {
+  value       = commandpersistence_cmd.terraform_module_versions
+  description = "The output of the commandpersistence_cmd resource "
 }
