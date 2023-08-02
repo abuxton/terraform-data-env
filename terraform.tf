62f150924279c1d12b0436abf56d0c316c05d0d2
Adam Buxton <buxton.adam@gmail.com>
2023-08-02 11:54:45 +0100

    add providers

--- terraform.tf
+++ terraform.tf
@@ -1,3 +1,23 @@
 terraform {
-  required_version = ">= 0.14"
+  required_version = ">= 1.5"
+
+  required_providers {
+    commandpersistence = {
+      source  = "terrycain/commandpersistence"
+      version = "1.1.0"
+    }
+    environment = {
+      source  = "EppO/environment"
+      version = "1.3.4"
+    }
+  }
 }
+
+provider "environment" {
+  # Configuration options
+}
+
+provider "commandpersistence" {
+  # Configuration options
+}
+
